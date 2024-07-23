2f821de75ac8a2ea6d2d72012ee81dcb59445dbf
Rahim <rahimullah25678@gmail.com>
2024-07-23 23:23:57 +0500

    here are the output of python file in different colors

---
+++ colored_output.py
@@ -0,0 +1,6 @@
+from colorama import Fore
+print(Fore.RED + "i am output with red color.")
+print(Fore.BLUE + "i am output with red color.")
+print(Fore.YELLOW + "i am output with red color.")
+print(Fore.GREEN + "i am output with red color.")
+print(Fore.CYAN + "i am output with red color.")
